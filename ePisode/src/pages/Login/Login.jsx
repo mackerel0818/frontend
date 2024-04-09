b8ae283be1e57d4b9436f6897df2bdf82ef3ab66
jindayeong <jindy0423@gmail.com>
2024-04-09 22:23:55 +0900

    Init - 로그인 초기

--- ePisode/src/pages/Login/Login.jsx
+++ ePisode/src/pages/Login/Login.jsx
@@ -1,5 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import style from './Login.module.css'
+
 
 export default function Login() {
-  return <div>Login</div>
+
+  return (
+    <>
+      <img className={style.logo} src='https://res.cloudinary.com/dnbf7czsn/image/upload/v1712585378/logo_on0pc8.png'></img>
+      <h2 className={style.Login}>LOGIN</h2>
+      <form>
+        <p>
+            <input className={style.email} type='email' name='email' placeholder='Email'/>
+        </p>
+        <p></p>
+        <p>
+            <input className={style.password} type='password' name='password' placeholder='Password'/>
+        </p>
+      </form>
+      <button className={style.button}>Login</button>
+      <button className={style.noId}>Don't have an account?</button>
+    </>
+  )
 }
