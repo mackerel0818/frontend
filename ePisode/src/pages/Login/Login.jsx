1776bcc0263e02193795451891a04245446103d9
jindayeong <jindy0423@gmail.com>
2024-04-10 19:38:15 +0900

    Update - 로고 클릭 시 메인으로 이동

--- ePisode/src/pages/Login/Login.jsx
+++ ePisode/src/pages/Login/Login.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from './Login.module.css'
 import { useNavigate } from 'react-router-dom'
 
 
 export default function Login() {
   const navigate = useNavigate();
+
+  const imgClick = () => {
+    navigate('/')
+  }
 
   const loginClick = () => {
     const isEmail = localStorage.getItem('isEmail')
@@ -13,7 +17,7 @@ export default function Login() {
     if(isEmail && isPassword) {
       navigate('/');
     } else if (isEmail === null || isPassword === null){
-      alert ("아이디 또는 비밀번호를 입력하세요.")
+      alert ("아이디와 비밀번호를 입력하세요.")
     } else{
       alert ("아이디 또는 비밀번호가 일치하지 않습니다.");
     }
@@ -22,22 +26,47 @@ export default function Login() {
   const noidClick = () => {
     navigate('/signup');
   }
- // TODO - 로고 클릭 시 메인 페이지로 이동
+
   return (
     <>
-      <img className={style.logo} src='https://res.cloudinary.com/dnbf7czsn/image/upload/v1712585378/logo_on0pc8.png'></img>
+      <img 
+        className={style.logo} 
+        src='https://res.cloudinary.com/dnbf7czsn/image/upload/v1712585378/logo_on0pc8.png'
+        onClick={imgClick}
+      >
+      </img>
       <h2 className={style.Login}>LOGIN</h2>
       <form>
         <p>
-            <input className={style.email} type='email' name='email' placeholder='Email'/>
+            <input 
+              className={style.email} 
+              type='email' 
+              name='email' 
+              placeholder='Email'
+            />
         </p>
         <p></p>
         <p>
-            <input className={style.password} type='password' name='password' placeholder='Password'/>
+            <input 
+              className={style.password} 
+              type='password' 
+              name='password' 
+              placeholder='Password'
+            />
         </p>
       </form>
-      <button className={style.button} onClick={loginClick}>Login</button>
-      <p className={style.noId} onClick={noidClick}>Don't have an account?</p>
+      <button 
+        className={style.button} 
+        onClick={loginClick}
+      >
+        Login
+      </button>
+      <p 
+        className={style.noId} 
+        onClick={noidClick}
+      >
+        Don't have an account?
+      </p>
     </>
   )
 }
