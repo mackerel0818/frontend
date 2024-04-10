fca55d418e8bb751b6e59729ab88d82e4825e393
makerel0818 <minji2700@naver.com>
2024-04-11 01:37:28 +0900

    Add - 카카오 지도 가져오기

--- ePisode/src/pages/MapPage/MapPage.jsx
+++ ePisode/src/pages/MapPage/MapPage.jsx
@@ -1,5 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function MapPage() {
-  return <div>map page</div>
+  const mapRef = React.useRef(null)
+
+  useEffect(() => {
+    const mapScript = document.createElement('script')
+
+    mapScript.async = true
+    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${import.meta.env.VITE_KAKAO_API_KEY}&autoload=false`
+
+    document.head.appendChild(mapScript)
+
+    const onLoadKakaoMap = () => {
+      window.kakao.maps.load(() => {
+        const mapContainer = mapRef.current
+        const mapOption = {
+          center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+          level: 3,
+        }
+        new window.kakao.maps.Map(mapContainer, mapOption)
+      })
+    }
+
+    window.onload = () => {
+      onLoadKakaoMap()
+    }
+
+    return () => {
+      window.onload = null
+    }
+  }, [])
+
+  return <div ref={mapRef} style={{ width: '100%', height: '100vh' }} id="map"></div>
 }
