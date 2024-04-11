8b55859b5f734c592edd2a05193387775969f313
makerel0818 <minji2700@naver.com>
2024-04-11 13:28:13 +0900

    Style - 로그인 페이지 스타일 변경

--- ePisode/src/pages/Login/Login.jsx
+++ ePisode/src/pages/Login/Login.jsx
@@ -5,21 +5,21 @@ import { useNavigate } from 'react-router-dom'
 //추후 삭제
 const User = {
   Email: '1234@gmail.com',
-  Password: '1234'
+  Password: '1234',
 }
 
 export default function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   const imgClick = () => {
     navigate('/')
   }
 
   const loginClick = () => {
-    if(email===User.Email && password===User.Password){
+    if (email === User.Email && password === User.Password) {
       navigate('/')
     } else if (email === null || password === null) {
       alert('아이디와 비밀번호를 입력해주세요.')
@@ -29,53 +29,30 @@ export default function Login() {
   }
 
   const noidClick = () => {
-    navigate('/signup');
+    navigate('/signup')
   }
 
   return (
-    <>
-      <img 
-        className={style.logo} 
-        src='https://res.cloudinary.com/dnbf7czsn/image/upload/v1712585378/logo_on0pc8.png'
-        onClick={imgClick}
-      >
-      </img>
-      <h2 className={style.Login}>LOGIN</h2>
-      <form>
-        <p>
-            <input 
-              className={style.email} 
-              type='text' 
-              name='email' 
-              placeholder='Email'
-              value={email}
-              onChange={(e)=>setEmail(e.target.value)}
-            />
+    <div className={style.wrap}>
+      <img className={style.logo} src="https://res.cloudinary.com/dnbf7czsn/image/upload/v1712585378/logo_on0pc8.png" onClick={imgClick}></img>
+      <section className={style.wrap_login}>
+        <h2 className={style.Login}>LOGIN</h2>
+        <form>
+          <p>
+            <input className={style.email} type="text" name="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          </p>
+          <p></p>
+          <p>
+            <input className={style.password} type="password" name="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          </p>
+        </form>
+        <button className={style.button} onClick={loginClick}>
+          Login
+        </button>
+        <p className={style.noId} onClick={noidClick}>
+          Don't have an account?
         </p>
-        <p></p>
-        <p>
-            <input 
-              className={style.password} 
-              type='password' 
-              name='password' 
-              placeholder='Password'
-              value={password}
-              onChange={(e)=>setPassword(e.target.value)}
-            />
-        </p>
-      </form>
-      <button 
-        className={style.button} 
-        onClick={loginClick}
-      >
-        Login
-      </button>
-      <p 
-        className={style.noId} 
-        onClick={noidClick}
-      >
-        Don't have an account?
-      </p>
-    </>
+      </section>
+    </div>
   )
 }
