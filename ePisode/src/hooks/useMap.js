23cef53dfb127e20a2dc77775993ab200f6a7994
makerel0818 <minji2700@naver.com>
2024-04-11 15:01:45 +0900

    Update - 현재 위치 받아오기

--- ePisode/src/hooks/useMap.js
+++ ePisode/src/hooks/useMap.js
@@ -11,12 +11,42 @@ const useMap = (mapRef, apiKey) => {
 
     const onLoadKakaoMap = () => {
       window.kakao.maps.load(() => {
-        const mapContainer = mapRef.current
-        const mapOption = {
-          center: new window.kakao.maps.LatLng(33.450701, 126.570667),
-          level: 3,
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(
+            (position) => {
+              const lat = parseFloat(position.coords.latitude.toFixed(4))
+              const lon = parseFloat(position.coords.longitude.toFixed(4))
+
+              const locPosition = new window.kakao.maps.LatLng(lat, lon)
+
+              const mapContainer = mapRef.current
+              const mapOption = {
+                center: locPosition,
+                level: 3,
+              }
+
+              const map = new window.kakao.maps.Map(mapContainer, mapOption)
+
+              const zoomControl = new window.kakao.maps.ZoomControl()
+              map.addControl(zoomControl, window.kakao.maps.ControlPosition.RIGHT)
+
+              var marker = new kakao.maps.Marker({
+                position: new kakao.maps.LatLng(lat, lon),
+                map: map,
+              })
+            },
+            (error) => {
+              console.error(error)
+            },
+            {
+              enableHighAccuracy: true, // 높은 정확도 요청
+              timeout: 15000, // 15초 내에 응답이 없으면 오류를 반환 (선택적)
+              maximumAge: 0, // 캐시된 위치 정보 무시, 항상 최신 정보 요청 (선택적)
+            },
+          )
+        } else {
+          console.error('이 브라우저에서는 현재 위치 표시 기능을 지원하지 않습니다.')
         }
-        new window.kakao.maps.Map(mapContainer, mapOption)
       })
     }
 
