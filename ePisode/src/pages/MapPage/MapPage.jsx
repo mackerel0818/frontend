2e70302f492f4bbd96163bf1e870972b54838997
makerel0818 <minji2700@naver.com>
2024-04-13 02:48:04 +0900

    Style - 전화번호 빈 값 처리

--- ePisode/src/pages/MapPage/MapPage.jsx
+++ ePisode/src/pages/MapPage/MapPage.jsx
@@ -19,7 +19,7 @@ export default function MapPage() {
           <div className={styles.selectedPlaceInfo}>
             <h2>선택된 장소: {selectedPlace.place_name}</h2>
             <p>주소: {selectedPlace.address_name}</p>
-            <p>전화번호: {selectedPlace.phone}</p>
+            <p>전화번호: {selectedPlace.phone || '정보 없음'}</p>
           </div>
         )}
       </div>
