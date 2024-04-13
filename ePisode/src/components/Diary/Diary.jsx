6de23a28f9a05aa527616ca4952fe0000a13773b
makerel0818 <minji2700@naver.com>
2024-04-14 00:56:00 +0900

    Update - 다이어리 애니메이션 추가

--- ePisode/src/components/Diary/Diary.jsx
+++ ePisode/src/components/Diary/Diary.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 import styles from './Diary.module.css'
 
 export default function Diary({ selectedPlace }) {
-  console.log(selectedPlace)
   return (
-    <div className={styles.selectedPlaceInfo}>
+    <motion.div
+      className={styles.selectedPlaceInfo}
+      initial={{ opacity: 0, x: 500 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{
+        duration: 0.5,
+      }}
+    >
       <h2>선택된 장소: {selectedPlace.place_name}</h2>
       <p>주소: {selectedPlace.address_name}</p>
       <p>전화번호: {selectedPlace.phone || '정보 없음'}</p>
-    </div>
+    </motion.div>
   )
 }
