346f610a0ce417774028ef5ca02527c9723e4b83
makerel0818 <minji2700@naver.com>
2024-04-14 09:03:41 +0900

    Update - 다이어리 레이아웃 구현

--- ePisode/src/components/Diary/Diary.jsx
+++ ePisode/src/components/Diary/Diary.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 import styles from './Diary.module.css'
+import { MdLocationOn } from 'react-icons/md'
 
 export default function Diary({ selectedPlace }) {
+  const categoryName = selectedPlace.category_name.split(' > ').pop()
   return (
     <motion.div
+      style={{
+        WebkitUserSelect: 'none' /* Safari */,
+        msUserSelect: 'none' /* Internet Explorer/Edge */,
+        userSelect: 'none' /* Non-prefixed version, currently supported by Chrome, Firefox, and Opera */,
+      }}
       className={styles.selectedPlaceInfo}
       initial={{ opacity: 0, x: 500 }}
       animate={{ opacity: 1, x: 0 }}
@@ -12,9 +19,30 @@ export default function Diary({ selectedPlace }) {
         duration: 0.5,
       }}
     >
-      <h2>선택된 장소: {selectedPlace.place_name}</h2>
-      <p>주소: {selectedPlace.address_name}</p>
-      <p>전화번호: {selectedPlace.phone || '정보 없음'}</p>
+      <section className={styles.image}></section>
+      <section className={styles.place_info}>
+        <div className={styles.wrap_name}>
+          <h2 className={styles.place_name}>{selectedPlace.place_name}</h2>
+          <p className={styles.category_name}>{categoryName}</p>
+        </div>
+        <p className={styles.address_name}>
+          <MdLocationOn className={styles.icon} />
+          {selectedPlace.road_address_name || selectedPlace.address_name}
+        </p>
+      </section>
+      <button className={styles.add_episode}>
+        <span className={styles.plus_btn}>+</span>
+      </button>
+      <section className={styles.diary}>
+        <ul className={styles.episodes}>
+          <li className={styles.episode}>EPISODE 1 - Lorem ipsum dolor sit a...</li>
+          <li className={styles.episode}>EPISODE 2 - Lorem ipsum dolor sit a...</li>
+          <li className={styles.episode}>EPISODE 3 - Lorem ipsum dolor sit a...</li>
+          <li className={styles.episode}>EPISODE 4 - Lorem ipsum dolor sit a...</li>
+          <li className={styles.episode}>EPISODE 5 - Lorem ipsum dolor sit a...</li>
+          <li className={styles.episode}>EPISODE 6 - Lorem ipsum dolor sit a...</li>
+        </ul>
+      </section>
     </motion.div>
   )
 }
