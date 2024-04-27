8cda560055dd8c005097e27df50e123eb12ca162
makerel0818 <minji2700@naver.com>
2024-04-28 00:52:42 +0900

    Style - 서치 카드 스타일 변경

--- ePisode/src/components/Card/SearchCard.jsx
+++ ePisode/src/components/Card/SearchCard.jsx
@@ -5,10 +5,8 @@ import styles from './SearchCard.module.css'
 export default function SearchCard({ index, place_name, category_name, road_address_name, address_name }) {
   return (
     <div key={index} className={styles.card}>
-      <div className={styles.wrap_title}>
-        <p className={styles.card_title}>{place_name}</p>
-        <p className={styles.card_category}>{category_name.split(' > ').pop()}</p>
-      </div>
+      <p className={styles.card_category}>{category_name.split(' > ').pop()}</p>
+      <p className={styles.card_title}>{place_name}</p>
       <p className={styles.address_name}>
         <MdLocationOn className={styles.icon} />
         {road_address_name || address_name}
