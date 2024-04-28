c1ceb0194de4cf538d2d0362cbcdd630b6eb2e46
makerel0818 <minji2700@naver.com>
2024-04-28 20:51:39 +0900

    Add - 추천 페이지 좋아요 버튼 추가

--- ePisode/src/components/Card/RecommendCard.jsx
+++ ePisode/src/components/Card/RecommendCard.jsx
@@ -1,21 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { MdLocationOn } from 'react-icons/md'
+import { GoHeart, GoHeartFill } from 'react-icons/go'
+
 import styles from './RecommendCard.module.css'
 import { useSelectedPlace } from '../../Contexts/SelectedPlaceContext'
 
 export default function RecommendCard({ index, place, place_name, category_name, road_address_name, address_name }) {
   const { setSelectedPlace } = useSelectedPlace()
+  const [liked, setLiked] = useState(false)
 
   const handleClick = () => {
     setSelectedPlace({ place })
   }
 
+  const handleLikeClick = (e) => {
+    e.stopPropagation()
+    setLiked(!liked)
+  }
+
   return (
     <div key={index} className={styles.card} onClick={handleClick}>
-      <p className={styles.card_category}>{category_name.split(' > ').pop()}</p>
-      <p className={styles.card_title}>{place_name}</p>
+      <div className={styles.wrap}>
+        <div>
+          <p className={styles.card_category}>{category_name.split(' > ').pop()}</p>
+          <p className={styles.card_title}>{place_name}</p>
+        </div>
+        <button className={styles.btn} onClick={handleLikeClick}>
+          {liked ? <GoHeartFill className={styles.icon_heart} /> : <GoHeart className={styles.icon_heart} />}
+        </button>
+      </div>
+
       <p className={styles.address_name}>
-        <MdLocationOn className={styles.icon} />
+        <MdLocationOn className={styles.icon_location} />
         {road_address_name || address_name}
       </p>
     </div>
