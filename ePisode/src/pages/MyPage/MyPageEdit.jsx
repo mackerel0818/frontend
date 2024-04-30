2d108c5e1e9230a6dd71465dcd7d9e5e65175030
jindayeong <jindy0423@gmail.com>
2024-04-30 23:51:15 +0900

    Add - 수정, 탈퇴 기능 추가

--- ePisode/src/pages/MyPage/MyPageEdit.jsx
+++ ePisode/src/pages/MyPage/MyPageEdit.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from './MyPageEdit.module.css'
 import { useNavigate } from 'react-router-dom'
 
@@ -13,19 +13,47 @@ export default function MyPageEdit() {
     e.stopPropagation()
   }
 
+  const saveClick = () => {
+    navigate('/map/mypage')
+  }
+
+  const deleteClick = () => {
+    if (window.confirm("탈퇴하시겠습니까?")) {
+      navigate("/")
+    } else {
+      alert("탈퇴 취소");
+    }
+  }
+  
+  //TODO  사진 업로드, mbti, 관심사 선택 창
+
   return (
     <div className={style.filter} onClick={handleClick}> 
       <div className={style.mywrap} onClick={handleInnerClick}>
         <p className={style.title}>사용자 정보 수정</p>
-        <div className={style.proCate}>
+        <div className={style.proList}>
             <p className={style.proHead}>프로필 사진</p>
-            <p className={style.proHead}>이름</p>
-            <p className={style.proHead}>MBTI</p>
+            <div className={style.group}>
+              <p className={style.proHead}>이름</p>
+              <input className={style.inputName}></input>
+            </div>
+            <div className={style.group}>
+              <p className={style.proHead}>MBTI</p>
+              <select className={style.selectMbti}></select>
+            </div>
+            
             <p className={style.proHead}>관심사</p>
-            <p className={style.proHead}>주소</p>
+            <div className={style.group}>
+              <p className={style.proHead}>주소</p>
+              <input className={style.inputAddress}></input>
+            </div>
+            
         </div>
-          
-
+        <div className={style.bottom}>
+          <button className={style.saveBtn} onClick={saveClick}>저장</button>
+          <button className={style.deleteUser} onClick={deleteClick}>탈퇴하기</button>
+        </div>
+        
       </div>
     </div>
   )
